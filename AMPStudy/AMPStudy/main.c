33b8504daf8711419799b3a2187a5092c50c2fc2
pilotns <pilot.ns@icloud.com>
2017-04-08 22:30:26 +0300

    AMPStudy - project has been created

---
+++ AMPStudy/AMPStudy/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  AMPStudy
+//
+//  Created by pilotns on 08.04.17.
+//  Copyright © 2017 pilotns. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
